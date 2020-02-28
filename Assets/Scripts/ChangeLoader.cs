7dd5b0928299ebff8b214dedefc27e0a13d60cec
Kevin Esaa <kevin.esaa@gmail.com>
2020-02-28 00:32:43 -0400

    save project

---
+++ Assets/Scripts/ChangeLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLoader : MonoBehaviour
+{
+    public const int GAME_SCENE = 1;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(GAME_SCENE);
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float force;
+    private Rigidbody2D mRigidbody;
+    
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            mRigidbody.AddForce(force * Vector2.up);
+        }
+    }
+}
